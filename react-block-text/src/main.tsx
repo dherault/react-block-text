5ce909713d2a65ad5d8673f51cf005659c8b0bd7
David Hérault <dherault@gmail.com>
2023-07-07 05:23:22 +0200

    Remove globalThis reference in main

--- react-block-text/src/main.tsx
+++ react-block-text/src/main.tsx
@@ -4,9 +4,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// @ts-ignore
-var global = globalThis // eslint-disable-line
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
