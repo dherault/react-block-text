c8614a412e2192fd0268efb7028def942cff8a33
David Hérault <dherault@gmail.com>
2023-08-03 07:22:42 +0200

    Remove react-dom useless dependency

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,16 +1,9 @@
-// import path from 'node:path'
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // resolve: {
-  //   alias: {
-  //     'react-block-text': path.resolve(__dirname, '../react-block-text'),
-  //   },
-  // },
   optimizeDeps: {
     exclude: ['react-block-text'],
   },
